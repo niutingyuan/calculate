364f30934f191639824d933a24f403ce7fec8c23
Thomas <thomasniu@gmail.com>
2024-02-22 14:02:25 +0800

    Use package big

--- internal/utils/factorial/factorial.go
+++ internal/utils/factorial/factorial.go
@@ -1,8 +1,19 @@
 package factorial
 
-func Calculate(n int) int {
-	if n == 0 {
-		return 1
+import (
+	"math/big"
+
+	"github.com/niutingyuan/calculate"
+)
+
+var _ calculate.FactorialCalculator = &Calculator{}
+
+type Calculator struct{}
+
+func (Calculator) Calculate(n int) *big.Int {
+	result := big.NewInt(1)
+	for i := 1; i <= n; i++ {
+		result.Mul(result, big.NewInt(int64(i)))
 	}
-	return n * Calculate(n-1)
+	return result
 }
